8edd9f96ed6a9acc202d78f122d4d6d4079f802b
yagoplx <57966308+yagoplx@users.noreply.github.com>
2020-04-21 02:34:20 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # AS-1.14.4/ObserverLib portable developer environment
-# Complies with HellFirePvP's LICENCE.
+# Complies with HellFirePvP's LICENSE.
 
 setup(){
     echo "AS-1.14.4/ObserverLib portable developer environment"
-    echo "Complies with HellFirePvP's LICENCE."
+    echo "Complies with HellFirePvP's LICENSE."
     echo "Grabbing code from upstream..."
     git clone https://github.com/HellFirePvP/AstralSorcery.git
     git clone https://github.com/HellFirePvP/ObserverLib.git
@@ -53,17 +53,22 @@ cp -rv build/libs/$(ls build/libs/ | grep -vi deobf | grep -vi sources) ../mc-li
 echo "Packaging Astral Sorcery to baked-jars folder"
 cp -rv build/libs/$(ls build/libs/ | grep -vi deobf | grep -vi sources) ../baked-jars/
 echo "Astral Sorcery is ready."
+cd ..
     fi
 
     if [ "$makeOL" == "true" ]; then
 echo "Making ObserverLib"
-#cp -rv gradle/* MirrorObserverLib
+cp -rv gradle/* MirrorObserverLib
+cd MirrorObserverLib
 ./gradlew build
 echo "Syncing src to git"
 cp -r src ../ObserverLib/
 echo "Packaging ObserverLib to test env" 
 cp -rv build/libs/$(ls build/libs/ | grep -vi deobf | grep -vi sources) ../mc-live/mods/ || echo "mc-live should point to your mods folder. Make a symlink!"
+echo "Packaging ObserverLib to baked-jars folder"
+cp -rv build/libs/$(ls build/libs/ | grep -vi deobf | grep -vi sources) ../baked-jars/
 echo "ObserverLib is ready."
+cd ..
     fi
 
 unset makeOL
